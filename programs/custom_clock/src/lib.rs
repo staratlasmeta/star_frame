af64bca6f8858125e8615e34272afc4c2a2e0fc4
Brett Etter <brett.etter@staratlas.com>
2023-05-22 13:31:25 -0500

    Bump to 0.1.3 and update cargo toml values

--- programs/custom_clock/src/lib.rs
+++ programs/custom_clock/src/lib.rs
@@ -1,3 +1,5 @@
+//! Custom clock settings for development.
+
 #![warn(missing_debug_implementations, clippy::pedantic)]
 #![allow(
     clippy::missing_errors_doc,
@@ -14,6 +16,7 @@ declare_id!("CLockX54wfiQwQoRGcMAxitSPKdNcSRousbcHEJoecou");
 pub mod custom_clock {
     use super::*;
 
+    /// Initializes a new clock.
     pub fn init_clock(ctx: Context<InitClock>, slot: u64, timestamp: i64) -> Result<()> {
         *ctx.accounts.clock.load_init()? = CustomClock {
             version: 0,
@@ -23,6 +26,7 @@ pub mod custom_clock {
         Ok(())
     }
 
+    /// Sets a clock.
     pub fn set_clock(ctx: Context<SetClock>, slot: u64, timestamp: i64) -> Result<()> {
         *ctx.accounts.clock.load_mut()? = CustomClock {
             version: 0,
@@ -33,29 +37,39 @@ pub mod custom_clock {
     }
 }
 
+/// A Custom clock that can be set by the program.
 #[derive(Debug)]
 #[account(zero_copy)]
 pub struct CustomClock {
+    /// The data version of the account. Should always be `0`.
     pub version: u8,
+    /// The slot for the clock.
     pub slot: u64,
+    /// The timestamp for the clock.
     pub timestamp: i64,
 }
 
+/// Sets a clock.
 #[derive(Accounts, Debug)]
 pub struct SetClock<'info> {
+    /// The clock to set.
     #[account(mut)]
     pub clock: AccountLoader<'info, CustomClock>,
 }
 
+/// Creates a new [`CustomClock`] account.
 #[derive(Accounts, Debug)]
 pub struct InitClock<'info> {
+    /// The funder for the new clock.
     #[account(mut)]
     pub funder: Signer<'info>,
+    /// The clock to create.
     #[account(
         init,
         payer = funder,
         space = 8 + 1 + 8 * 2,
     )]
     pub clock: AccountLoader<'info, CustomClock>,
+    /// The system program.
     pub system_program: Program<'info, System>,
 }
