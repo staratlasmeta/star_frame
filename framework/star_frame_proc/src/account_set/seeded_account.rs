1f2ffec289dd69bc9636b71ef08cf6955296b85f
bryanzierk <bryan.zierk@gmail.com>
2024-06-27 10:27:06 -0600

    Strip out duplicate struct

--- framework/star_frame_proc/src/account_set/seeded_account.rs
+++ framework/star_frame_proc/src/account_set/seeded_account.rs
@@ -3,41 +3,16 @@ use easy_proc::find_attr;
 use proc_macro2::{Ident, Span, TokenStream};
 use proc_macro_error::abort;
 use quote::quote;
-use syn::{Attribute, Data, DeriveInput, Expr, Generics, Visibility};
-
-#[allow(dead_code)]
-struct StrippedDeriveInput {
-    attrs: Vec<Attribute>,
-    vis: Visibility,
-    ident: Ident,
-    generics: Generics,
-}
+use syn::{Data, DeriveInput, Expr};
 
 pub fn derive_get_seeds_impl(input: DeriveInput) -> TokenStream {
-    let out = match input.data {
-        Data::Struct(s) => derive_get_seeds_impl_struct(
-            Paths::default(),
-            s,
-            StrippedDeriveInput {
-                attrs: input.attrs,
-                vis: input.vis,
-                ident: input.ident,
-                generics: input.generics,
-            },
-        ),
+    let data_struct = match input.data {
+        Data::Struct(s) => s,
         Data::Enum(e) => abort!(e.enum_token, "GetSeeds cannot be derived for enums"),
         Data::Union(u) => abort!(u.union_token, "GetSeeds cannot be derived for unions"),
     };
 
-    out
-}
-
-fn derive_get_seeds_impl_struct(
-    paths: Paths,
-    data_struct: syn::DataStruct,
-    input: StrippedDeriveInput,
-) -> TokenStream {
-    let Paths { get_seeds, .. } = paths;
+    let Paths { get_seeds, .. } = Paths::default();
 
     let ident = &input.ident;
     let (impl_generics, type_generics, where_clause) = input.generics.split_for_impl();
