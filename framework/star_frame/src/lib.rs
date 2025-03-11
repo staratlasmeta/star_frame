92cb1fedc0ed8a8433bea0c9bbf702df734f400b
Sammy Harris <sammy@stegabob.com>
2025-03-11 17:54:33 -0500

    remove core extern...?

--- framework/star_frame/src/lib.rs
+++ framework/star_frame/src/lib.rs
@@ -30,7 +30,6 @@ pub extern crate advance;
 pub extern crate anyhow;
 pub extern crate borsh;
 pub extern crate bytemuck;
-extern crate core;
 pub extern crate derivative;
 pub extern crate derive_more;
 pub extern crate fixed;
