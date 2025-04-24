c3dbefbb692c1e1d9195c483c0cf63befa2a7f7c
Sammy Harris <sammy@stegabob.com>
2025-04-24 14:21:01 -0500

    make test helpers static again...

--- star_frame/src/unsize/test_helpers.rs
+++ star_frame/src/unsize/test_helpers.rs
@@ -52,25 +52,25 @@ impl<'info> UnsizedTypeDataAccess<'info> for TestUnderlyingData<'info> {
 
 /// A way to work with [`UnsizedType`] types off-chain. Uses a [`TestUnderlyingData`] internally.
 #[derive(Debug)]
-pub struct TestByteSet<'info, T: ?Sized + UnsizedType> {
-    test_data: &'info TestUnderlyingData<'info>,
-    // test_data_ptr: *mut TestUnderlyingData<'info>,
+pub struct TestByteSet<T: ?Sized + UnsizedType> {
+    test_data: *mut TestUnderlyingData<'static>,
     data_ptr: *mut Vec<u8>,
     phantom_t: PhantomData<T>,
 }
 
-impl<T: ?Sized + UnsizedType> Drop for TestByteSet<'_, T> {
+impl<T: ?Sized + UnsizedType> Drop for TestByteSet<T> {
     fn drop(&mut self) {
-        drop(unsafe { Box::from_raw(core::ptr::from_ref(self.test_data).cast_mut()) });
+        drop(unsafe { Box::from_raw(self.test_data) });
         drop(unsafe { Box::from_raw(self.data_ptr) });
     }
 }
 
-impl<'info, T> TestByteSet<'info, T>
+impl<T> TestByteSet<T>
 where
     T: UnsizedType + ?Sized,
 {
     fn initialize(size: usize, init: impl FnOnce(&mut &mut [u8]) -> Result<()>) -> Result<Self> {
+        // Temporarily leak the data. It will be cleaned up in the Drop implementation.
         let data: &mut Vec<u8> = Box::leak(Box::default());
         let data_ptr: *mut Vec<u8> = data;
         let test_account = Box::leak(Box::new(TestUnderlyingData::new(data, size)));
@@ -85,6 +85,14 @@ where
             phantom_t: PhantomData,
             data_ptr,
         })
+    }
+
+    /// # Safety
+    ///
+    /// We need to ensure this value is never exposed to the user, since it could outlive Self and lead
+    /// to use after free errors.
+    unsafe fn test_data_ref(&self) -> &'static TestUnderlyingData<'static> {
+        unsafe { &*self.test_data }
     }
 
     /// Creates a new [`TestByteSet`] from the owned value
@@ -113,20 +121,24 @@ where
         Self::new_from_init(DefaultInit)
     }
 
-    pub fn data<'a>(&'a self) -> Result<SharedWrapper<'a, 'info, T::Ref<'info>>> {
-        unsafe { SharedWrapper::<T>::new(self.test_data) }
+    pub fn data(&self) -> Result<SharedWrapper<'_, 'static, T::Ref<'static>>> {
+        unsafe { SharedWrapper::<T>::new(self.test_data_ref()) }
     }
 
-    pub fn data_mut<'a>(&'a self) -> Result<MutWrapper<'a, 'info, T, TestUnderlyingData<'info>>> {
-        unsafe { MutWrapper::new(self.test_data) }
+    pub fn data_mut(&self) -> Result<MutWrapper<'_, 'static, T, TestUnderlyingData<'static>>> {
+        unsafe { MutWrapper::new(self.test_data_ref()) }
     }
 
     pub fn owned(&self) -> Result<T::Owned> {
-        T::owned(&self.test_data.data.try_borrow()?)
+        // Safety: test_data is not being returned to the user
+        let test_data = unsafe { self.test_data_ref() };
+        T::owned(&test_data.data.try_borrow()?)
     }
 
     pub fn underlying_data(&self) -> Result<Vec<u8>> {
-        Ok(self.test_data.data.try_borrow()?.to_vec())
+        // Safety: test_data is not being returned to the user
+        let test_data = unsafe { self.test_data_ref() };
+        Ok(test_data.data.try_borrow()?.to_vec())
     }
 }
 
@@ -153,14 +165,14 @@ macro_rules! assert_eq_with_shared {
 }
 
 pub trait NewByteSet: UnsizedType {
-    fn new_byte_set<'info>(owned: Self::Owned) -> Result<TestByteSet<'info, Self>>
+    fn new_byte_set(owned: Self::Owned) -> Result<TestByteSet<Self>>
     where
         Self: FromOwned,
     {
         TestByteSet::<Self>::new(owned)
     }
 
-    fn new_default_byte_set<'info>() -> Result<TestByteSet<'info, Self>>
+    fn new_default_byte_set() -> Result<TestByteSet<Self>>
     where
         Self: UnsizedInit<DefaultInit>,
     {
@@ -171,10 +183,10 @@ pub trait NewByteSet: UnsizedType {
 impl<T> NewByteSet for T where T: UnsizedType + ?Sized {}
 
 pub trait ModifyOwned: Clone {
-    fn modify_owned<'info, U>(
+    fn modify_owned<U>(
         &mut self,
         modify: impl FnOnce(
-            &mut ExclusiveWrapperT<'_, '_, '_, 'info, U, U, TestUnderlyingData<'info>>,
+            &mut ExclusiveWrapperT<'_, '_, '_, 'static, U, U, TestUnderlyingData<'static>>,
         ) -> Result<()>,
     ) -> Result<()>
     where
