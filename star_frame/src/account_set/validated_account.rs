9b4960b2a09c95541aa4f494ed78400a8f1720d7
Sammy Harris <sammy@stegabob.com>
2025-06-02 12:05:15 -0500

    16 should be enough in a single tuple...

--- star_frame/src/account_set/validated_account.rs
+++ star_frame/src/account_set/validated_account.rs
@@ -46,4 +46,4 @@ macro_rules! account_validate_tuple {
     }
 }
 
-account_validate_tuple!(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z);
+account_validate_tuple!(A B C D E F G H I J K L M N O P);
