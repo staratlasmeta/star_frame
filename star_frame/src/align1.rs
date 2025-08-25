59aba101245c16b64e3599bfb11fbb10b7c94129
Sammy Harris <41593264+stegaBOB@users.noreply.github.com>
2025-08-25 12:03:08 -0500

    better safety and module docs (#204)

--- star_frame/src/align1.rs
+++ star_frame/src/align1.rs
@@ -1,4 +1,4 @@
-//! Alignment to 1 byte.
+//! Alignment to 1 byte. Much of the [`crate::unsize`] magic relies on packed alignment and no padding.
 
 use solana_pubkey::Pubkey;
 pub use star_frame_proc::Align1;
@@ -7,7 +7,7 @@ use std::{
     num::{NonZeroI8, NonZeroU8},
 };
 
-/// A trait for types that are guaranteed to be aligned to 1 byte. The [unsized type system](crate::unsize) relies on `Align1` types for its pointer manipulation.
+/// A marker trait for types that are guaranteed to be aligned to 1 byte. The [unsized type system](crate::unsize) relies on `Align1` types for its pointer manipulation.
 ///
 /// # Safety
 /// This trait should only be implemented for types that are guaranteed to be aligned to 1 byte.
@@ -17,7 +17,8 @@ pub unsafe trait Align1 {}
 macro_rules! impl_align1 {
     ($($name:ty),*) => {
         $(
-            // Safety: Allowed due to the lower assert.
+            // SAFETY:
+            // Allowed due to the lower assert.
             unsafe impl $crate::align1::Align1 for $name {}
             $crate::static_assertions::assert_eq_align!($name, u8);
         )*
@@ -26,9 +27,12 @@ macro_rules! impl_align1 {
 
 impl_align1!((), u8, i8, bool, Pubkey, NonZeroU8, NonZeroI8);
 
-// Safety: Allowed because `PhantomData` is a ZST.
+// SAFETY:
+// Allowed because `PhantomData` is a ZST.
 unsafe impl<T: ?Sized> Align1 for PhantomData<T> {}
-// Safety: Allowed because a slice of `T` is aligned to `T`.
+// SAFETY:
+// Allowed because a slice of `T` is aligned to `T`.
 unsafe impl<T> Align1 for [T] where T: Align1 {}
-// Safety: Allowed because an array of `T` is aligned to `T`.
+// SAFETY:
+// Allowed because an array of `T` is aligned to `T`.
 unsafe impl<T, const N: usize> Align1 for [T; N] where T: Align1 {}
