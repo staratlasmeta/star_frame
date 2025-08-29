4b8f52ef034549d628db68a3eaa6781011d2bb0a
Ubadineke Prince <90358880+ubadineke@users.noreply.github.com>
2025-08-29 17:21:56 +0100

    feat: Add CLI with project scaffolding (#227)
    This PR introduces a new command-line interface(CLI) along with a project scaffolding system for quickly generating new Solana programs.
    
    **Features:** 
    Subcommands:
    
    - `new`: Generates a full project scaffold including:
      - Base directories (src, instructions, tests)
      - Essential template files (Cargo.toml, .gitignore, lib.rs, instruction modules, and test files)
    - `build`: compiling programs
    - `test`: running tests 
      - Supports `--nocapture` flag to display test output if necessary
    
    The scaffold generator enhances adoption and accessibility by giving developers a quick start. It includes a counter program that can be easily modified. 
    While the team may not have planned a CLI, I implemented one because no existing structure supported this feature. The scaffold generator depends on it to function properly.

---
+++ cli/README.md
@@ -0,0 +1,11 @@
+# Star_Frame_Cli
+
+## Usage
+
+### Create a new project
+
+Scaffold a new Solana program project:
+
+```bash
+star_frame_cli new <PROJECT-NAME>
+```
---
+++ cli/src/main.rs
@@ -0,0 +1,22 @@
+use clap::{command, Parser, Subcommand};
+pub mod new_project;
+use new_project::*;
+
+#[derive(Subcommand, Debug)]
+enum CliCommand {
+    #[command(about = "Create new Solana program")]
+    New(NewArgs),
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: CliCommand,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        CliCommand::New(args) => new_project(args),
+    }
+}
